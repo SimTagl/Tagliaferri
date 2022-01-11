f45f06fc418f9b7b98be26ec1284fc2869a67c27
SimTagl <simone.tagliaferri.s27@liceoviconapoli.it>
2022-01-11 13:22:03 +0100

    Update thislist.py

--- thislist.py
+++ thislist.py
@@ -1,8 +1,8 @@
-thislist= ["apple, banana, cherry"]
+thislist= ["apple", "banana", "cherry"]
 list2= [1, 5, 7, 9, 3]
 list3= [True, False, False]
 list4= ["abc",34, True, 40, "male"]
-print(thislist)
-print(list2)
+print(len(thislist))
+print(type(list2))
 print(list3)
 print(list4)
